7cf90b4b4e0c7b5eb071f99174dddd9cad17281a
g10albert <albertmiguelg10@gmail.com>
2025-11-03 21:47:13 -0400

    Completed the Learning activity for week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Designer";
+        job1._startYear = 2025;
+        job1._endYear = 2025;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "YouTube";
+        job2._jobTitle = "Programmer";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Albert";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+public class Resume
+{
+  public string _name = "";
+  public List<Job> _jobs = new List<Job>();
+
+  public void Display()
+  {
+    Console.WriteLine($"Nmae: {_name}");
+    Console.WriteLine("Jobs:");
+    foreach (Job a in _jobs)
+    {
+      Console.WriteLine($"{a._company}");
+    }
+  }
+}
