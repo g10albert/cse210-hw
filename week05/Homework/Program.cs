0031eab691e48acd7fc8756380419696fcdcc192
g10albert <albertmiguelg10@gmail.com>
2025-11-24 22:37:43 -0400

    Completed the learning activities for week 5

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment();
+
+        a1.setName("Albert");
+        a1.SetTopic("Math");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment a2 = new MathAssignment("Albert", "Math", "7.1", "1-4");
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Jose", "Tech", "Technology in the future");
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
